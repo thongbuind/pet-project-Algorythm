d3d7def0355ee9b426e5f5348007285b677369ef
thongbuind <thongbuind2005@gmail.com>
2024-09-08 07:54:51 +0700

    update renderPost

--- edit.js
+++ edit.js
@@ -43,39 +43,31 @@ function renderPost(element) {
     var html = `
         <form action="" id="edit-form" method="POST">
             <div class="edit-header">Lý thuyết</div>
-            <textarea id="theory-title-1" name="theory-title-1" wrap="soft">${element["theory-title-1"]}</textarea>
-            <textarea id="theory-p-1" name="theory-p-1" wrap="soft">${element["theory-p-1"]}</textarea>
-            <textarea id="theory-p-2" name="theory-p-2" wrap="soft">${element["theory-p-2"]}</textarea>
-            <textarea id="theory-title-2" name="theory-title-2" wrap="soft">${element["theory-title-2"]}</textarea>
-            <textarea id="theory-p-3" name="theory-p-3" wrap="soft">${element["theory-p-3"]}</textarea>
-            <textarea id="theory-p-4" name="theory-p-4" wrap="soft">${element["theory-p-4"]}</textarea>
-            <textarea id="theory-title-3" name="theory-title-3" wrap="soft">${element["theory-title-3"]}</textarea>
-            <textarea id="theory-p-5" name="theory-p-5" wrap="soft">${element["theory-p-4"]}</textarea>
-            <textarea id="theory-p-6" name="theory-p-6" wrap="soft">${element["theory-p-5"]}</textarea>
-            <textarea id="theory-title-4" name="theory-title-4" wrap="soft">${element["theory-title-4"]}</textarea>
-            <textarea id="theory-p-7" name="theory-p-7" wrap="soft">${element["theory-p-7"]}</textarea>
-            <textarea id="theory-p-8" name="theory-p-8" wrap="soft">${element["theory-p-8"]}</textarea>
-
-            <div class="edit-header">Ví dụ</div>
-            <textarea id="example-1-name" name="example-1-name" class="ex-name" wrap="soft">${element["example-1-name"]}</textarea>
-            <textarea id="example-1-depcription-1" name="example-1-depcription-1" class="ex-depcription" wrap="soft">${element["example-1-depcription-1"]}</textarea>
-            <textarea id="example-1-depcription-2" name="example-1-depcription-2" class="ex-depcription" wrap="soft">${element["example-1-depcription-2"]}</textarea>
-            <textarea id="example-1-depcription-3" name="example-1-depcription-3" class="ex-depcription" wrap="soft">${element["example-1-depcription-3"]}</textarea>
-            <textarea id="example-1-depcription-4" name="example-1-depcription-4" class="ex-depcription" wrap="soft">${element["example-1-depcription-4"]}</textarea>
-            <textarea id="example-1-depcription-5" name="example-1-depcription-5" class="ex-depcription" wrap="soft">${element["example-1-depcription-5"]}</textarea>
-            <textarea id="example-2-name" name="example-2-name" class="ex-name" wrap="soft">${element["example-2-name"]}</textarea>
-            <textarea id="example-2-depcription-1" name="example-2-depcription-1" class="ex-depcription" wrap="soft">${element["example-2-depcription-1"]}</textarea>
-            <textarea id="example-2-depcription-2" name="example-2-depcription-2" class="ex-depcription" wrap="soft">${element["example-2-depcription-2"]}</textarea>
-            <textarea id="example-2-depcription-3" name="example-2-depcription-3" class="ex-depcription" wrap="soft">${element["example-2-depcription-3"]}</textarea>
-            <textarea id="example-2-depcription-4" name="example-2-depcription-4" class="ex-depcription" wrap="soft">${element["example-2-depcription-4"]}</textarea>
-            <textarea id="example-2-depcription-5" name="example-2-depcription-5" class="ex-depcription" wrap="soft">${element["example-2-depcription-5"]}</textarea>
-            <textarea id="example-3-name" name="example-3-name" class="ex-name" wrap="soft">${element["example-3-name"]}</textarea>
-            <textarea id="example-3-depcription-1" name="example-3-depcription-1" class="ex-depcription" wrap="soft">${element["example-3-depcription-1"]}</textarea>
-            <textarea id="example-3-depcription-2" name="example-3-depcription-2" class="ex-depcription" wrap="soft">${element["example-3-depcription-2"]}</textarea>
-            <textarea id="example-3-depcription-3" name="example-3-depcription-3" class="ex-depcription" wrap="soft">${element["example-3-depcription-3"]}</textarea>
-            <textarea id="example-3-depcription-4" name="example-3-depcription-4" class="ex-depcription" wrap="soft">${element["example-3-depcription-4"]}</textarea>
-            <textarea id="example-3-depcription-5" name="example-3-depcription-5" class="ex-depcription" wrap="soft">${element["example-3-depcription-5"]}</textarea>
-
+    `;
+    for (let i=0; i<element["theory"].length; i++) {
+        html += `
+            <textarea id="theory-title-${i+1}" name="theory-title-${i+1}" wrap="soft">${element["theory"][i]["title"]}</textarea>
+        `;
+        for (let j=0; j<element["theory"][i]["content"].length; j++) {
+            html += `
+                <textarea id="theory-p-${j+1}" name="theory-p-${j+1}" wrap="soft">${element["theory"][i]["content"][j]}</textarea>
+            `;
+        }
+    }
+    html += `
+        <div class="edit-header">Ví dụ</div>
+    `;
+    for (let i=0; i<element["examples"].length; i++) {
+        html += `
+            <textarea id="example-${i+1}-name" name="example-${i+1}-name" class="ex-name" wrap="soft">${element["examples"][i]["name"]}</textarea>
+        `;
+        for (let j=0; j<element["examples"][i]["description"].length; j++) {
+            html += `
+                <textarea id="theory-p-${j+1}" name="theory-p-${j+1}" wrap="soft">${element["examples"][i]["description"][j]}</textarea>
+            `;
+        }
+    }
+    html += `
             <div id="edit-back">Back</div>
             <button type="" id="edit-push">Push</buttonnnnn>
         </form>
